f0fee812b4b201c7b8d17bb699a9585a15c1fef3
Marcel Martin <marcel.martin@scilifelab.se>
2022-02-19 01:04:52 +0100

    Test on more Python versions

--- data/download.sh
+++ data/download.sh
@@ -10,12 +10,19 @@ set -x
 
 # 11387212 reads, 43 bp single-end
 fastq-dump -F --defline-qual '+' SRR020285
+mv SRR020285.fastq single43bp.fastq
 
 # 2 x 150 bp paired-end, first 2 million reads
 fastq-dump --split-3 --maxSpotId 2000000 --defline-seq '@$ac.$si' --defline-qual '+' SRR12131341
+mv SRR12131341_1.fastq paired150bp_1.fastq
+mv SRR12131341_2.fastq paired150bp_2.fastq
+
 
 # 1380851 reads, 305 bp paired-end, MiSeq
 fastq-dump --split-3 -F --defline-qual '+' ERR1760498
+mv ERR1760498_1.fastq paired300bp_1.fastq
+mv ERR1760498_2.fastq paired300bp_2.fastq
 
 # 115769 reads, avg. length 9024 bp, PacBio
 fastq-dump --defline-seq '@$ac.$si' --defline-qual '+' SRR2174302
+mv SRR2174302.fastq single9000bp.fastq
