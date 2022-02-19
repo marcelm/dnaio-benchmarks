c38c8c6714bfdadc65eef99f6875511a669ee9bb
Marcel Martin <marcel.martin@scilifelab.se>
2022-02-19 01:15:37 +0100

    More benchmarks

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -6,13 +6,24 @@ def data_path(name):
     return os.path.join(os.environ["ASV_CONF_DIR"], "data", name)
 
 
-def time_parse_SRR020285():
-    with dnaio.open(data_path("SRR020285.fastq")) as f:
+def time_parse_single_43bp(path):
+    with dnaio.open(path) as f:
         for record in f:
             pass
 
 
-def time_SRR020285_sequence_attribute():
-    with dnaio.open(data_path("SRR020285.fastq")) as f:
-        for record in f:
-            len(record.sequence)
+time_parse_single_43bp.params = [
+    data_path(path + ".fastq") for path in
+    ["single43bp", "paired150bp_1", "paired300bp_1", "single9000bp"]
+]
+time_parse_single_43bp.param_names = ["dataset"]
+
+
+class Suite:
+    def setup(self):
+        self.record = dnaio.Sequence("record_name", "ACGT" * 25, "=->!" * 25)
+
+    def time_sequence_attribute(self):
+        r = self.record
+        for _ in range(100_000):
+            r.sequence
