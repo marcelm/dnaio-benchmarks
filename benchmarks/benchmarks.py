71baa92ef84665a614d693535983e3b897c24859
Marcel Martin <marcel.martin@scilifelab.se>
2022-02-19 07:54:44 +0100

    Paired-end parsing

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -3,20 +3,33 @@ import dnaio
 
 
 def data_path(name):
-    return os.path.join(os.environ["ASV_CONF_DIR"], "data", name)
+    return os.path.join(os.environ["ASV_CONF_DIR"], "data", name) + ".fastq"
 
 
-def time_parse_single_43bp(path):
-    with dnaio.open(path) as f:
+def time_parse_single(dataset):
+    with dnaio.open(data_path(dataset)) as f:
         for record in f:
             pass
 
 
-time_parse_single_43bp.params = [
-    data_path(path + ".fastq") for path in
-    ["single43bp", "paired150bp_1", "paired300bp_1", "single9000bp"]
+time_parse_single.params = [
+    "single43bp", "paired150bp_1", "paired300bp_1", "single9000bp"
 ]
-time_parse_single_43bp.param_names = ["dataset"]
+time_parse_single.param_names = ["dataset"]
+
+
+def time_parse_paired(path):
+    with dnaio.open(
+        data_path(path + "_1"),
+        file2=data_path(path + "_2"),
+    ) as f:
+        for record in f:
+            pass
+
+time_parse_paired.params = [
+    "paired150bp", "paired300bp"
+]
+time_parse_single.param_names = ["dataset"]
 
 
 class Suite:
@@ -25,5 +38,5 @@ class Suite:
 
     def time_sequence_attribute(self):
         r = self.record
-        for _ in range(100_000):
+        for _ in range(1_000_000):
             r.sequence
