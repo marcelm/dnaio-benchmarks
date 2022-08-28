c55786fdaba53542805bb5b72f9ea0061e82e675
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2022-08-28 08:09:15 +0200

    Complete reading program

--- comparisons/fastp/read.cpp
+++ comparisons/fastp/read.cpp
@@ -1,12 +1,19 @@
 #include "fastp/src/fastqreader.h"
-#include <errno.h>
+#include "fastp/src/read.h"
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        fprintf(stderr, "Input and output file must be given");
+    if (argc != 1) {
+        fprintf(stderr, "Input file must be given");
         return 1;
     }
     char *in_file = argv[0];
-    char *out_file = argv[1];
+    FastqReader reader = FastqReader(in_file);
+    Read *record;
+    while (1) {
+        record = reader.read();
+        if (record == NULL) {
+            return 0;
+        }
+    return 0;
 }
