265822fb8b06c0937bd5944d7c6ad4878b9dfcbc
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2022-08-28 07:51:01 +0200

    Write to filehandle

--- comparisons/dnaio/read_and_write.py
+++ comparisons/dnaio/read_and_write.py
@@ -5,6 +5,6 @@ import sys
 
 if __name__ == "__main__":
     with dnaio.open(sys.argv[1], mode="r") as in_records:
-        with dnaio.open(sys.argv[2], mode="w", fileformat="fastq") as out_file:
-            for record in in_records:
-                out_file.write(record)
+        with open(sys.argv[2], mode="wb") as out_file:
+            for record in in_records:  # dnaio.SequenceRecord
+                out_file.write(record.fastq_bytes())
