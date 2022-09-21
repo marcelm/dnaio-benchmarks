a5f1f74738bb64d1b1dd6d41ad486d3ef6f0fb83
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2022-09-21 14:54:25 +0200

    Make sure dnaio is single-threaded

--- comparisons/dnaio/read_and_write.py
+++ comparisons/dnaio/read_and_write.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 
+import functools
+import xopen
 import dnaio
 import sys
 
+# Open single threaded for a fair comparison with other tools.
+OPENER = functools.partial(xopen.xopen, threads=0)
+
 if __name__ == "__main__":
-    with dnaio.open(sys.argv[1], mode="r") as in_records:
+    with dnaio.open(sys.argv[1], mode="r", opener=OPENER) as in_records:
         with open(sys.argv[2], mode="wb") as out_file:
             for record in in_records:  # dnaio.SequenceRecord
                 out_file.write(record.fastq_bytes())
--- comparisons/dnaio/read_paired.py
+++ comparisons/dnaio/read_paired.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
 
+import functools
+import xopen
 import dnaio
 import sys
 
+# Open single threaded for a fair comparison with other tools.
+OPENER = functools.partial(xopen.xopen, threads=0)
+
 if __name__ == "__main__":
-    with dnaio.open(sys.argv[1], file2=sys.argv[1], mode="r") as records:
+    with dnaio.open(sys.argv[1], file2=sys.argv[1], mode="r", opener=OPENER) as records:
         for record in records:
             pass
